2668e386a29968dc7b680f7d4d9bea23a8c29216
fhusain01 <fhusain@adaptready.com>
2023-08-28 23:45:51 +0530

    React query setup

--- src/App.js
+++ src/App.js
@@ -3,32 +3,35 @@ import './App.css'
 import { HomePage } from './Components/Home.page'
 import { RQSuperHeroesPage } from './Components/RQSuperHeroes.page'
 import { SuperHeroesPage } from './Components/SuperHeroes.page'
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+// Initialze the client
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/super-heroes'>Traditional Super Heroes</Link>
-          </li>
-          <li>
-            <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/super-heroes' element={<SuperHeroesPage />} />
-        <Route path='/rq-super-heroes' element={<RQSuperHeroesPage />} />
-      </Routes>
-
-
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/super-heroes'>Traditional Super Heroes</Link>
+            </li>
+            <li>
+              <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/super-heroes' element={<SuperHeroesPage />} />
+          <Route path='/rq-super-heroes' element={<RQSuperHeroesPage />} />
+        </Routes>
+      </div>
+    </QueryClientProvider>
 
 
   )

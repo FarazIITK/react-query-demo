f6ca8ac4d15c5ef53be9279246f5dd64d2f65653
FarazIITK <fhusain@wr.iitr.ac.in>
2023-08-29 12:04:35 +0530

    Added react-query dev tool

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { Routes, Route, Link } from 'react-router-dom'
-import './App.css'
-import { HomePage } from './Components/Home.page'
-import { RQSuperHeroesPage } from './Components/RQSuperHeroes.page'
-import { SuperHeroesPage } from './Components/SuperHeroes.page'
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import { HomePage } from "./Components/Home.page";
+import { RQSuperHeroesPage } from "./Components/RQSuperHeroes.page";
+import { SuperHeroesPage } from "./Components/SuperHeroes.page";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 // Initialze the client
 const queryClient = new QueryClient();
@@ -15,26 +16,25 @@ function App() {
         <nav>
           <ul>
             <li>
-              <Link to='/'>Home</Link>
+              <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to='/super-heroes'>Traditional Super Heroes</Link>
+              <Link to="/super-heroes">Traditional Super Heroes</Link>
             </li>
             <li>
-              <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
             </li>
           </ul>
         </nav>
         <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/super-heroes' element={<SuperHeroesPage />} />
-          <Route path='/rq-super-heroes' element={<RQSuperHeroesPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/super-heroes" element={<SuperHeroesPage />} />
+          <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
         </Routes>
       </div>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
-
-
-  )
+  );
 }
 
-export default App
+export default App;
